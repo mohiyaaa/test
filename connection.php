ddbb020e3c57ff5a29a9bb34c91187a44ffe427d
mohiyaaa <mohiyaaa@gmail.com>
2019-07-18 11:29:41 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,22 @@
+<?php
+
 $conn=new MySQLi("localhost","root","","test");
 mysqli_select_db($conn,"test");
-mysqli_query($conn,"SELECT * FROM `id` ");
+$response=array();
+$result=mysqli_query($conn,"SELECT * FROM `id` ");
+if(MySQLi_num_rows($result)>0){
+  while($row=MySQLi_fetch_array($result)){
+    $temp=array();
+    $temp["name"]=$row["name"];
+    $response["travel"]=array();
+    array_push($response["travel"],$temp);
+  }
+  $response["t"]=1;
+  echo json_encode($response);
+}else{
+  $response["t"]=0;
+  $response["message"]="not found";
+  echo json_encode($response);
+}
+
+?>
