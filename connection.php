34c9ce95df5012d58170b5bdf4c554094260cebb
mohiyaaa <mohiyaaa@gmail.com>
2019-07-19 02:43:11 -0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,22 +1,29 @@
 <?php
 
-$conn=new MySQLi("127.0.0.1","root","","test");
-mysqli_select_db($conn,"test");
+$conn=new MySQLi("localhost","root","","testt");
+mysqli_select_db($conn,"testt");
 $response=array();
-$result=mysqli_query($conn,"SELECT * FROM `id` ");
-if(MySQLi_num_rows($result)>0){
-  while($row=MySQLi_fetch_array($result)){
+if($result = mysqli_query($conn,"SELECT * FROM `id` ")){
+	$row_cnt = $result->num_rows;
+	printf("Result set has %d rows.\n", $row_cnt);
+
+	
+  while($row = $result->fetch_array(MYSQLI_BOTH)){
     $temp=array();
-    $temp["name"]=$row["name"];
+    $temp['name']=$row['name'];
     $response["travel"]=array();
     array_push($response["travel"],$temp);
   }
   $response["t"]=1;
   echo json_encode($response);
 }
+
 else{
   $response["t"]=0;
   $response["message"]="not found";
   echo json_encode($response);
 }
+$result->close();
+$conn->close();
+
 ?>
