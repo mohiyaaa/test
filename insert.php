c923e3182265e08538ff92475a23ce4da7d1ac2a
mohiyaaa <mohiyaaa@gmail.com>
2019-07-29 01:44:25 -0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,9 +1,9 @@
 <?php
    
-   $json =$_POST['name'];
-    $name1 = json_decode($json);
-	$name= $name1->{'name'};
-    $conn=new MySQLi("localhost","root","","testt");
+   $name =$_POST['name'];
+    //$name1 = json_decode($json);
+	//$name= $name1->{'name'};
+    $conn=new MySQLi("Localhost","root","","testt");
     mysqli_select_db($conn,"testt");
 	$query="INSERT INTO `id`(`name`) VALUES ('".$name."');";
 	mysqli_query($conn,$query);
